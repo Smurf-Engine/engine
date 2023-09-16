85183e4c5067adae5f7e268a2bcb4817137d6d23
siddastic <siddhantgaurav2003@gmail.com>
2023-09-16 13:30:27 +0530

    clear canvas and redraw logic

--- src/engine/smurf_engine.ts
+++ src/engine/smurf_engine.ts
@@ -4,7 +4,9 @@ import { Settings } from "./settings";
 export class SmurfEngine {
     previousTime = Date.now();
     private scene? : Scene;
+    private readonly cx : CanvasRenderingContext2D;
     constructor(public canvas: HTMLCanvasElement) {
+        this.cx = canvas.getContext("2d")!;
         this.run();
     }
 
@@ -12,8 +14,17 @@ export class SmurfEngine {
         let newTime = Date.now();
         Settings.add('dt', (newTime - this.previousTime) / 1000);
         this.previousTime = newTime;
+
+        // clear and redraw
+        this.clearCanvas();
         this.scene?.render();
+
+
         requestAnimationFrame(this.run);
+    }
+
+    clearCanvas(){
+        this.cx.clearRect(0,0,this.canvas.width,this.canvas.height);
     }
 
     loadScene(scene : Scene){
