ef14c5af00fad444f183819d2cb3dfc86e9e7a62
siddastic <siddhantgaurav2003@gmail.com>
2023-10-17 20:23:23 +0530

    fix bug with gravity

--- src/engine/input_system.ts
+++ src/engine/input_system.ts
@@ -1,76 +1,76 @@
 import { autobind } from "../data/autobind";
 import { Vector2 } from "../data/vector2";
 
-export class Input{
-    private pressedKeyCodes: string[] = [];
-    // mouse
-    boundingClientRect: DOMRect = new DOMRect(0,0,0,0);
-    private mousePosition : Vector2 = Vector2.zero;
-    private downMouseButtons : number[] = [];
-    constructor(canvas: HTMLCanvasElement) {
-        addEventListener("keydown", this.onKeyDown);
-        addEventListener("keyup", this.onKeyUp);
-        // mouse events
-        canvas.addEventListener("mousemove", this.onMouseMove);
-        // get bounding client rect for relative mouse position
-        this.boundingClientRect = canvas.getBoundingClientRect();
-        canvas.addEventListener("mousedown", this.onMouseDown);
-        canvas.addEventListener("mouseup", this.onMouseUp);
-        // prevent right click context menu
-        canvas.addEventListener("contextmenu", (e) => e.preventDefault());
-    }
+export class Input {
+  private pressedKeyCodes: string[] = [];
+  // mouse
+  boundingClientRect: DOMRect = new DOMRect(0, 0, 0, 0);
+  private mousePosition: Vector2 = Vector2.zero;
+  private downMouseButtons: number[] = [];
+  constructor(canvas: HTMLCanvasElement) {
+    addEventListener("keydown", this.onKeyDown);
+    addEventListener("keyup", this.onKeyUp);
+    // mouse events
+    canvas.addEventListener("mousemove", this.onMouseMove);
+    // get bounding client rect for relative mouse position
+    this.boundingClientRect = canvas.getBoundingClientRect();
+    canvas.addEventListener("mousedown", this.onMouseDown);
+    canvas.addEventListener("mouseup", this.onMouseUp);
+    // prevent right click context menu
+    canvas.addEventListener("contextmenu", (e) => e.preventDefault());
+  }
 
-    @autobind
-    private onKeyDown(e: KeyboardEvent) {
-        // add key if not present
-        if (this.pressedKeyCodes.find((k) => k == e.code) == undefined) {
-            this.pressedKeyCodes.push(e.code);
-        }
+  @autobind
+  private onKeyDown(e: KeyboardEvent) {
+    // add key if not present
+    if (this.pressedKeyCodes.find((k) => k == e.code) == undefined) {
+      this.pressedKeyCodes.push(e.code);
     }
+  }
 
-    @autobind
-    private onKeyUp(e: KeyboardEvent) {
-        // remove key if present
-        let index = this.pressedKeyCodes.findIndex((k) => k == e.code);
-        if (index == -1) return;
-        this.pressedKeyCodes.splice(index, 1);
-    }
+  @autobind
+  private onKeyUp(e: KeyboardEvent) {
+    // remove key if present
+    let index = this.pressedKeyCodes.findIndex((k) => k == e.code);
+    if (index == -1) return;
+    this.pressedKeyCodes.splice(index, 1);
+  }
 
-    isPressed(key: string): boolean {
-        return this.pressedKeyCodes.find((k) => k == key) != undefined;
-    }
+  isPressed(key: string): boolean {
+    return this.pressedKeyCodes.find((k) => k == key) != undefined;
+  }
 
-    @autobind
-    private onMouseMove(e: MouseEvent) {
-        this.mousePosition = new Vector2(e.clientX - this.boundingClientRect.left, e.clientY - this.boundingClientRect.top);
-    }
+  @autobind
+  private onMouseMove(e: MouseEvent) {
+    this.mousePosition = new Vector2(e.clientX - this.boundingClientRect.left, e.clientY - this.boundingClientRect.top);
+  }
 
-    getMousePosition() : Vector2{
-        return this.mousePosition;
-    }
+  getMousePosition(): Vector2 {
+    return this.mousePosition;
+  }
 
-    get mouseX() : number{
-        return this.mousePosition.x;
-    }
+  get mouseX(): number {
+    return this.mousePosition.x;
+  }
 
-    get mouseY() : number{
-        return this.mousePosition.y;
-    }
+  get mouseY(): number {
+    return this.mousePosition.y;
+  }
 
-    @autobind
-    private onMouseDown(e: MouseEvent) {
-        e.preventDefault();
-        this.downMouseButtons.push(e.button);
-    }
+  @autobind
+  private onMouseDown(e: MouseEvent) {
+    e.preventDefault();
+    this.downMouseButtons.push(e.button);
+  }
 
-    @autobind
-    private onMouseUp(e: MouseEvent) {
-        let index = this.downMouseButtons.findIndex((b) => b == e.button);
-        if (index == -1) return;
-        this.downMouseButtons.splice(index, 1);
-    }
+  @autobind
+  private onMouseUp(e: MouseEvent) {
+    let index = this.downMouseButtons.findIndex((b) => b == e.button);
+    if (index == -1) return;
+    this.downMouseButtons.splice(index, 1);
+  }
 
-    getMouseKeyDown(button: number): boolean {
-        return this.downMouseButtons.find((b) => b == button) != undefined;
-    }
+  getMouseKeyDown(button: number): boolean {
+    return this.downMouseButtons.find((b) => b == button) != undefined;
+  }
 }
--- src/example/block_world.ts
+++ src/example/block_world.ts
@@ -2,24 +2,55 @@ import { engine } from "./setup";
 import { Scene } from "../engine/scene";
 import GameObject from "../game_object/game_object";
 import { Vector2 } from "../data/vector2";
-import { SpriteRenderer } from "../game_object/components/sprite_renderer";
 import { BoxRenderer } from "../game_object/components/box_renderer";
 import { Physics2D } from "../game_object/components/physics2d";
 import { CanvasBoxBoundsCollider } from "../game_object/components/box_bounds_collider";
 import Component from "../game_object/component";
 
+class PlayerMovement extends Component {
+  physics2d!: Physics2D;
+  start(): void {
+    this.gameObject.getComponent<BoxRenderer>(BoxRenderer)!.color = "white";
+    this.physics2d = this.gameObject.getComponent<Physics2D>(Physics2D)!;
+  }
+  update(): void {
+    if (this.input.isPressed("ArrowUp") || this.input.isPressed("Space")) {
+      this.physics2d.velocity.y = -5;
+    }
+    if (this.input.isPressed("ArrowLeft") || this.input.isPressed("KeyA")) {
+      this.physics2d.velocity.x = -5;
+    }
+    else if (this.input.isPressed("ArrowRight") || this.input.isPressed("KeyD")) {
+      this.physics2d.velocity.x = 5;
+    } else {
+      this.physics2d.velocity.x = 0;
+    }
+
+    console.log(this.physics2d.velocity);
+  }
+}
+
 const blockWorld = new Scene();
 
-let background = new GameObject({
+const background = new GameObject({
   name: "Background",
   engine: engine,
 });
-
 background.transform.size = new Vector2(engine.canvas.width, engine.canvas.height);
+background.addComponent<BoxRenderer>(BoxRenderer).color = "magenta";
 
-let renderer = background.addComponent<BoxRenderer>(BoxRenderer);
-renderer.color = "magenta";
+const player = new GameObject({
+  name: "Player",
+  engine: engine,
+});
 
-blockWorld.addGameObject(background);
+player.transform.size = new Vector2(50, 50);
+player.transform.position = new Vector2(100, 100);
+player.addComponent(Physics2D);
+player.addComponent(BoxRenderer);
+player.addComponent(CanvasBoxBoundsCollider);
+player.addComponent(PlayerMovement);
+
+blockWorld.addGameObject(background, player);
 
 export { blockWorld };
